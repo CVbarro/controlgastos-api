36e3f935a106885a9065461d1340b99a8a18658b
CVbarro <cesarvianabarros@gmail.com>
2026-06-12 21:45:01 -0300

    (END): Api finalizada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import categoria_router,gastos_router
 
@@ -6,6 +7,16 @@ app = FastAPI(
     title="Controle de Gastos API",
     version="1.0.0"
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(categoria_router)
 app.include_router(gastos_router)
